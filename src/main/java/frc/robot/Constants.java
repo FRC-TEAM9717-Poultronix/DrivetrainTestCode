d61ffa15d95a0d2c2c0bf4aa969c4f57e68f9487
Chuck Ellison <chuckellison@hotmail.com>
2025-02-17 10:20:53 -0600

    Add elevator subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -18,30 +18,50 @@ import swervelib.math.Matter;
  */
 public final class Constants
 {
-
   public static final double ROBOT_MASS = (148 - 20.3) * 0.453592; // 32lbs * kg per pound
   public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
   public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
-  public static final double MAX_SPEED  = Units.feetToMeters(14.5);
-  // Maximum speed of the robot in meters per second, used to limit acceleration.
+  public static final double MAX_SPEED  = Units.feetToMeters(14.5);// Maximum speed of the robot in meters per second, used to limit acceleration.
 
-//  public static final class AutonConstants
-//  {
-//
-//    public static final PIDConstants TRANSLATION_PID = new PIDConstants(0.7, 0, 0);
-//    public static final PIDConstants ANGLE_PID       = new PIDConstants(0.4, 0, 0.01);
-//  }
+  public static final class ElevatorConstants
+  {
+    public static final int leftElevatorID = 11;  // Can Id of Left elevator motor
+    public static final int rightElevatorID = 12; // Can Id of Right elevator motor
+
+    public static final double maxOutput = 1.0;   // Max power output of elevator motors
+    public static final double maxVelocity = 1.0; // Max velocity of elevator
+    public static final double maxAcceleration = 1.0; // Max acceleration of elevator
+    public static final double countsPerInch = 1.0;   // Encoder ticks per inch of elevator travel
+    public static final double posTolerance = 0.5;    // Tolerance for PID control of elevator
+
+    // PID values
+    public static final double kP = 0.002;
+    public static final double kI = 0.0;
+    public static final double kD = 0.0;
+
+    // Feedforward values
+    public static final double kS = 0.002;
+    public static final double kV = 0.0;
+    public static final double kG = 0.0;
+    
+    // Stored elevator positions
+    public static final double minPos = 0;
+    public static final double downPos = 1.0;
+    public static final double L1 = 2.0;
+    public static final double L2 = 3.0;
+    public static final double L3 = 4.0;
+    public static final double L4 = 5.0;
+    public static final double maxPos = 10.0;
+  }
 
   public static final class DrivebaseConstants
   {
-
     // Hold time on motor brakes when disabled
     public static final double WHEEL_LOCK_TIME = 10; // seconds
   }
 
   public static class OperatorConstants
   {
-
     // Joystick Deadband
     public static final double DEADBAND        = 0.1;
     public static final double LEFT_Y_DEADBAND = 0.1;
