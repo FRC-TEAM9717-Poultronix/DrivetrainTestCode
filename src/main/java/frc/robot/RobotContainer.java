c9e445d09e88160e49c0108b6f1160676f25ff6c
Sam <sambradshaw2007@icloud.com>
2025-03-01 18:10:37 -0600

    more tuning :D

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -240,7 +240,7 @@ public class RobotContainer
         // Floor
         m_driver2Xbox.pov(180).onTrue(new ElevatorPosition(m_elevator, Constants.ElevatorConstants.positionFloor, m_coral, Constants.CoralConstants.positionUp, m_algae, Constants.AlgaeArmConstants.positionFloor));
         // net
-        m_driver2Xbox.button(8).onTrue(new ElevatorPositionAfterHome(m_elevator, Constants.ElevatorConstants.positionNet, m_coral, Constants.CoralConstants.positionUp, m_algae, Constants.AlgaeArmConstants.positionNet ));
+        m_driver2Xbox.button(8).onTrue(new ElevatorPosition(m_elevator, Constants.ElevatorConstants.positionNet, m_coral, Constants.CoralConstants.positionUp, m_algae, Constants.AlgaeArmConstants.positionNet ));
 
       // m_driver2Xbox.button(8).onTrue(hook down); 
    // m_driver2Xbox.button(9).onTrue(coral intake); 
